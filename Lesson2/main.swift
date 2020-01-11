01c891892cbef24bf361aa7966e18f6542daed7e
ruslzsv <ruslzsv@gmail.com>
2020-01-11 20:41:55 +0300

    initial commit

---
+++ Lesson2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson2
+//
+//  Created by Руслан Застава on 11.01.2020.
+//  Copyright © 2020 Руслан Застава. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
